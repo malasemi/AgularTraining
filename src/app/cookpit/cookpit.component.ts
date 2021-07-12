34884446f4bf1a85b624b43038ee530a621e7aa6
malasemi <malasemi@student.cscc.edu>
2021-07-12 19:10:30 -0400

    home 7-12

--- src/app/cookpit/cookpit.component.ts
+++ src/app/cookpit/cookpit.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-cookpit',
@@ -9,25 +9,30 @@ export class CookpitComponent implements OnInit {
   @Output() serverCreated = new EventEmitter<{serverName: string, serverContent: string}>();
   @Output('bpCreated') blueprintCreated = new EventEmitter<{serverName: string, serverContent: string}>();
   // we emit these value to the parent which is the app.component
-  newServerName = '';
-  newServerContent = '';
+  
+  //newServerName = ''; // we don't need this now because we use local reference
+  
+  // newServerContent = ''; // trying to access this var from everywhere else
 
+  @ViewChild('serverContentInput', {static: true}) serverContentInput: ElementRef
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  onAddServer() {
+  onAddServer(nameInput: HTMLInputElement) {
+    // console.log(this.serverContentInput);
+    // console.log(nameInput.value);
     this.serverCreated.emit({
-      serverName: this.newServerName, 
-      serverContent: this.newServerContent
+      serverName: nameInput.value, 
+      serverContent: this.serverContentInput.nativeElement.value
     });
   }
 
-  onAddBlueprint() {
+  onAddBlueprint(nameInput: HTMLInputElement) {
     this.blueprintCreated.emit({
-      serverName: this.newServerName, 
-      serverContent: this.newServerContent
+      serverName: nameInput.value, 
+      serverContent: this.serverContentInput.nativeElement.value
     });
   }
 
--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,16 +1,80 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { 
+  AfterViewChecked,
+  AfterContentChecked, 
+  AfterContentInit, 
+  AfterViewInit, 
+  Component, 
+  DoCheck, 
+  Input, 
+  OnChanges, 
+  OnInit, 
+  SimpleChanges, 
+  ViewEncapsulation,
+  OnDestroy,
+  ViewChild,
+  ElementRef,
+  ContentChild} from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
-  styleUrls: ['./server-element.component.css']
+  styleUrls: ['./server-element.component.css'],
+  encapsulation: ViewEncapsulation.Emulated // None, Native
 })
-export class ServerElementComponent implements OnInit {
-  @Input('srvElement') element: {type: string, name: string, content: string};// we take this value from the parent component
 
-  constructor() { }
+export class ServerElementComponent implements 
+OnInit, 
+OnChanges, 
+DoCheck, 
+AfterContentInit,
+AfterContentChecked,
+AfterViewInit,
+AfterViewChecked,
+OnDestroy
+ {
+  @Input('srvElement') element: { type: string, name: string, content: string };// we take this value from the parent component
+  @Input() name: string;
+  @ViewChild('heading', {static: true}) header: ElementRef;
+  @ContentChild('contentParagraph', {static: true}) paragraph: ElementRef;
+  
+  constructor() { 
+    console.log('constructor called!');
+  }
 
-  ngOnInit(): void {
+  ngOnChanges(changes: SimpleChanges) {
+    console.log('ngOnChanges called!');
+    console.log(changes);    
+    console.log('Paragraph element reference' + this.paragraph.nativeElement.textContent);
+  }
+
+  ngOnInit() {
+    console.log('ngOnInit called!');
+    console.log('Text Content ' + this.header.nativeElement.textContent);
+  }
+
+  ngDoCheck() {
+    console.log('ngDoCheck called!');    
+  }
+
+  ngAfterContentInit() {
+    console.log('ngAfterContentInit Called!');
+    console.log('Paragraph element reference ' + this.paragraph.nativeElement.textContent);
+  }
+
+  ngAfterContentChecked() {
+    console.log('ngAfterContentChecked Called!');    
+  }
+
+  ngAfterViewInit() {
+    console.log('ngAfterViewInit Called!');
+    console.log('Text Content ' + this.header.nativeElement.textContent);
+  }
+
+  ngAfterViewChecked() {
+    console.log('ngAfterViewChecked Called!');    
+  }
+  ngOnDestroy() {
+    console.log('ngOnDestroy Called!');    
   }
 
 }
